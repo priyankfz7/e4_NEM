38155fb7527e00051a1b579ea44da1a6fb017e24
priyankfz7 <110036030+priyankfz7@users.noreply.github.com>
2023-01-16 17:38:22 +0530

    e

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require("express");
 const { connection } = require("./Config/db");
 const { authentication } = require("./Middleware/authentication.middleware");
 const { postRouter } = require("./Routes/post.routes");
-
+const cors = require("cors");
 const { userRouter } = require("./Routes/users.routes");
 
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Social Media API");
+});
+app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
 app.use(authentication);
 app.use("/posts", postRouter);
-
-app.get("/", (req, res) => {
-  res.send("Social Media API");
-});
 
 app.listen(8080, async () => {
   try {
